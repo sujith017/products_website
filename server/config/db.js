995d4378e8d867fccf8753eb64e04cf5081c0222
sujith017 <sujithb934@gmail.com>
2024-11-18 21:26:39 +0530

    some changes

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+dotenv.config();
+
 export const connectdb = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
